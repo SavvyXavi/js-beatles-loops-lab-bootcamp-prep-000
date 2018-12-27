d55e0e2c2d42e00df0d17a4686e2ffa2026cdb46
Xavier Johnson <letitbexavi@outlook.com>
2018-12-27 02:44:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // add solution here
+function theBeatlesPlay (musicians, instruments) {
+  var emptyArray = [];
+  for (var i = 0; musicians.length < 5; i++) {
+    emptyArray.push(musicians[i] + ' plays ' + instruments[i]);
+  }
+  return emptyArray;
+}
